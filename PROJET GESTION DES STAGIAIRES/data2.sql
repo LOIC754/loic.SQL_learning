9a5b793b0a9c8b5a2030bd7c2ca0175d9f87d97f
AKAMGA K Loic <loicakamga@gmail.com>
2026-03-21 01:30:37 +0100

    update table enregistrement

--- PROJET GESTION DES STAGIAIRES/data2.sql
+++ PROJET GESTION DES STAGIAIRES/data2.sql
@@ -2,6 +2,7 @@
 ---CREATION DES VUES 
 
 --selection des stagiairs actifs 
+---- ce sont des tables virtiuels 
 SET SEARCH_PATH="RH"
 CREATE VIEW "VW_STAGIAIRE_ACTIF" AS 
 SELECT * FROM "TB_STAGIAIRES" WHERE "BL_ACTIF";
@@ -9,3 +10,12 @@ SELECT * FROM "TB_STAGIAIRES" WHERE "BL_ACTIF";
 --selection des stagiairs non actifs 
 CREATE VIEW "VW_STAGIAIRE_NON_ACTIF" AS 
 SELECT * FROM "TB_STAGIAIRES" WHERE NOT "BL_ACTIF";
+
+-----table N°4 TB_CONSTART 
+--modifier les donne des deja presentes dans une base de ou update 
+UPDATE "TB_CONTART" CTR 
+SET "DT_ARRET_CONTRAT" ='20223-05-03',
+    "LB_MOTIF_ARRET" ='absences repetées'
+WHERE "ID_CONTRAT" ='CSTG001';
+
+SELECT * FROM "TB_CONTART" ORDER BY "ID_CONTRAT"
